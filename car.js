cd11297568005248eb66948fdeea616f999386bb
t.kobayashi <taichiro.kobayashi@fujifilm.com>
2021-06-03 17:38:23 +0900

    セッション２までの結果をコミット

---
+++ car.js
@@ -0,0 +1,5 @@
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    return Car;
+}());
---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var hello = 'hello';
+console.log(hello);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
