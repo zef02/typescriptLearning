8e4cd1c21795e8a2f50f2bbd78d04efd105c4734
t.kobayashi <taichiro.kobayashi@fujifilm.com>
2021-06-04 16:42:25 +0900

    セッション２終了までのソースをコミット

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
-"use strict";
-exports.__esModule = true;
-var hello = 'hello';
-console.log(hello);
-function add(a, b) {
-    return a + b;
+var hasValue = false;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = 'hello';
+var double = "hello";
+var back = 'hello';
+var person = {
+    name: {
+        first: 'Jack',
+        last: 'Hawood'
+    },
+    age: 21
+};
+var fruits = ['Apple', 'Banana', 'Grape', 1];
+var book = ['business', 1500, false];
+book.push(21);
+var CoffeeSize;
+(function (CoffeeSize) {
+    CoffeeSize["SHORT"] = "Short";
+    CoffeeSize["TALL"] = "Tall";
+    CoffeeSize["GRANDE"] = "Grande";
+})(CoffeeSize || (CoffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: CoffeeSize.SHORT
+};
+console.log(coffee.size);
+var anytime = true;
+var unionType = 10;
+// unionType.toUpperCase();
+unionType = 'Hello';
+unionType.toUpperCase();
+// Literal型
+var apple = 'apple';
+// union + Literal
+var clothSize = 'medium';
+var clothSize2 = 'medium';
+var cloth = {
+    color: 'white',
+    size: clothSize
+};
+function add(num1, num2) {
+    return num1 + num2;
 }
-console.log(add(1, 2));
+function sayHello() {
+    console.log('Hello');
+}
+console.log(sayHello());
+var tmp;
+// 関数型の例
+var anotherAdd = add;
+var doubleNumber = function (number) { return number * 2; };
+console.log(doubleNumber(2));
--- index.ts
+++ index.ts
@@ -1,12 +1,81 @@
-import axios from 'axios';
+let hasValue = false;
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
 
-let hello: string = 'hello';
-console.log(hello);
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = 'hello';
+
+const person= {
+    name: {
+            first: 'Jack',
+            last: 'Hawood'
+    },
+    age: 21
+}
+const fruits = ['Apple', 'Banana', 'Grape', 1]
+
+const book: [string, number, boolean] = ['business', 1500, false];
+book.push(21)
+
+enum CoffeeSize {
+    SHORT = 'Short',
+    TALL = 'Tall',
+    GRANDE = 'Grande'
+}
+const coffee ={
+    hot: true,
+    size: CoffeeSize.SHORT
+
+}
+console.log(coffee.size)
 
 
-function add(a: number, b:number): number{
-    return a + b;
+let anytime:any = true;
+
+let unionType: string | number = 10;
+// unionType.toUpperCase();
+
+unionType = 'Hello';
+unionType.toUpperCase();
+
+// Literal型
+const apple: 'apple' = 'apple';
+
+type ClothSize = 'small' | 'medium' | 'Large'
+// union + Literal
+let clothSize: 'small' | 'medium' | 'Large' = 'medium'; 
+let clothSize2: ClothSize = 'medium'; 
+
+const cloth = {
+    color: 'white',
+    size: clothSize
 }
 
 
-console.log(add(1,2)); 
+function add(num1:number , num2:number): number{
+   return num1 + num2; 
+}
+
+
+function sayHello(): void{
+    console.log('Hello');
+}
+
+console.log(sayHello());
+let tmp: undefined;
+
+// 関数型の例
+const anotherAdd = add;
+
+const doubleNumber = number => {return number * 2}
+console.log(doubleNumber(2));
+
+function doubleAndHandle(num: number, cb:(num: number) => number): void {
+    const doubleNum = cb(num *2);
+    console.log(doubleNum);
+}
+doubleAndHandle(21,doubleNum => {
+    return doubleNum;
+});
